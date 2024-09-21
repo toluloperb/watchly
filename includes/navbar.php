fe78875f6a49292deb6b06f89c27ec22b22aac44
Damilare <sdesparkflux@gmail.com>
2024-09-21 02:44:50 +0100

    First Commit

---
+++ includes/navbar.php
@@ -0,0 +1,7 @@
+<nav>
+    <a href=""><img src="assets/image/watchly logo.png" alt=""></a>
+
+    <div class="menu-list">
+        <a href=""><p>Sign in</p></a>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    include("includes/header.php");
+    include("includes/navbar.php");
+?>
+
+<section class="backgroundImg">
+    <h1>Unlimited movies, TV shows, and more</h1>
+    <p>Starts at ₦2,200. Cancel anytime.</p>
+</section>
